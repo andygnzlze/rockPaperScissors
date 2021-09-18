8943a338ac648c3f023e081e2a8f223e5393ba19
Andy Gonzalez <andygnzlze@gmail.com>
2021-09-18 09:45:51 -0700

    tried adding while loop

--- java.js
+++ java.js
@@ -14,31 +14,38 @@ function computerPlay() {
     } 
 }
 
-// keep score first to 5
-function updateComputerScore() {
-    for(i=0)
-}
-
-function play() {
+function play(playerScore, computerScore) {
     //person types their answer rock, paper, or scissors
     computerPlay();
     console.log(computerSelection)
     let playerSelection = prompt('Pick your weapon', 'rock');
     if ( computerSelection == playerSelection) {
         alert('there was a draw');
+        return playerScore +=0
     }
     else if (computerSelection == 'rock' && playerSelection == 'scissors') {
         alert(`You lose, ${computerSelection} beats ${playerSelection}`);
+        return computerScore +=1
     }
     else if (computerSelection == 'paper' && playerSelection == 'rock') {
        alert(`You lose, ${computerSelection} beats ${playerSelection}`);
+       return computerScore += 1
     }
     else if (computerSelection == 'scissors' && playerSelection == 'paper') {
        alert(`You lose, ${computerSelection} beats ${playerSelection}`);
+       return computerScore += 1
     }
     else {
-        alert(`You win, ${playerSelection} beats ${computerSelection} `);
+        alert(`You win, ${playerSelection} beats ${computerSelection}`);
+        return playerScore +=1
     }
 }
 
-
+function playRound(playerScore, computerScore){
+    playerScore = 0;
+    computerScore = 0;
+    while (playerScore <=3 || computerScore <=3) {
+        play(playerScore, computerScore);
+        console.log(`the score is Player: ${playerScore} to Computer: ${computerScore}`);
+    }
+}
